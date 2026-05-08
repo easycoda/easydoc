6bdd70963218b00477a22bb49850bb8b5d714820
easycoda <easycoda@users.noreply.github.com>
2026-05-08 08:59:09 +0000

    deploy: 03e04ba772a95482b2c3d731957a5e6dd0567814

---
+++ assets/channel-DN-LtkXj.js
@@ -0,0 +1,1 @@
+import{aq as o,ar as n}from"./mermaid.core-BsYS8SBR.js";const t=(r,a)=>o.lang.round(n.parse(r)[a]);export{t as c};
---
+++ assets/useDocumentTitle-CIYWUnPu.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-BPryiN1_.js";function r(t){e.useEffect(()=>{document.title=t},[t])}export{r as u};
