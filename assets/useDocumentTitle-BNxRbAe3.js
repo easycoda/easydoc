69c4c613573d5327839b23d327f29dd693605639
easycoda <easycoda@users.noreply.github.com>
2026-05-09 14:11:41 +0000

    deploy: 93b4f772c60ccec3da818fc207219c754ac3a9dd

---
+++ assets/useDocumentTitle-BNxRbAe3.js
@@ -0,0 +1,1 @@
+import{r as e}from"./vendor-react-DbQfIqoz.js";function r(t){e.useEffect(()=>{document.title=t},[t])}export{r as u};
--- assets/useDocumentTitle-Bx7rOFpH.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./vendor-react-CRZOZWvA.js";function r(t){e.useEffect(()=>{document.title=t},[t])}export{r as u};
